dabfea9d4c5cff7439ea097107ff04fdb9bed364
thcl-odoo <thcl@odoo.com>
2025-01-08 15:43:18 +0100

    [ADD] odoo: add croissantage module

---
+++ odoo/croissantage/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Croissantage',
+    "version": "18.0.1.0.0",
+    'website': 'https://www.odoo.com/',
+    'summary': 'Handle your croissantages like never before and never miss a croissant again.',
+    'description': """Croissants ftw.""",
+    'depends': [
+        'contacts',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/croissantage.xml',
+        'views/res_partner.xml'
+    ],
+    'installable': True,
+    'auto_install': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
