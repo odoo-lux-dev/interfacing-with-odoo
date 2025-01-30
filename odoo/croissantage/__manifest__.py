397f0c4e2d094c4dd9024a5945e5b60aafab2a56
thcl-odoo <thcl@odoo.com>
2025-01-30 16:02:55 +0100

    [IMP] odoo: add webhook to add log note on croissantage

--- odoo/croissantage/__manifest__.py
+++ odoo/croissantage/__manifest__.py
@@ -6,12 +6,15 @@
     'description': """Croissants ftw.""",
     'depends': [
         'contacts',
+        'web_studio',
     ],
     'data': [
         'security/ir.model.access.csv',
         'views/croissantage.xml',
         'views/res_partner.xml',
         'data/mail_template_data.xml',
+        'data/base_automation.xml',
+        'data/ir_actions_server.xml',
     ],
     'installable': True,
     'auto_install': True,
